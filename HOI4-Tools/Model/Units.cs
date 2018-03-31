0ed192609e9c130e5ef02662cb68c10d9c1f84fd
FaleshSB <web@johnbaldock.com>
2018-03-31 14:50:21 +0100

    Started getting info from paradox files

--- HOI4-Tools/Model/Units.cs
+++ HOI4-Tools/Model/Units.cs
@@ -8,42 +8,144 @@ namespace HOI4_Tools.Model
 {
     public class Units
     {
+        string[] infantry;
+
+        int infantryStart;
+        int bicycleBattalionStart;
+        int marineStart;
+        int mountaineersStart;
+        int paratrooperStart;
+        int motorizedStart;
+        int mechanizedStart;
+
+        int infantryEnd;
+        int bicycleBattalionEnd;
+        int marineEnd;
+        int mountaineersEnd;
+        int paratrooperEnd;
+        int motorizedEnd;
+        int mechanizedEnd;
+
+        bool isCheckingInfantry = false;
+        bool isCheckingBicycleBattalion = false;
+        bool isCheckingMarine = false;
+        bool isCheckingMountaineers = false;
+        bool isCheckingParatrooper = false;
+        bool isCheckingMotorized = false;
+        bool isCheckingMechanized = false;
+
         public Units()
         {
-            string[] infantry = FileHandler.LoadFile("infantry.txt");
-            int infantryStart;
-            int infantryEnd;
-            int bicycleBattalionStart;
-            int bicycleBattalionEnd;
+            GetData();
+            int i = 0;
+        }
 
-            bool isCheckingInfantry = false;
-            bool isCheckingbicycleBattalion = false;
+        private void GetData()
+        {
+            infantry = FileHandler.LoadFile("infantry.txt");
+            GetInfantryLocations();
+            GetInfantryStats();
+        }
 
-            for (int i = 0;i < infantry.Length;i++)
+        private void GetInfantryStats()
+        {
+            Unit basicInfantry = new Unit();
+            for (int i = 0; i < infantry.Length; i++)
             {
-                if(infantry[i].Contains("infantry = {"))
+                if (infantry[i].Contains("infantry = {"))
+                {
+
+                }
+        }
+
+        private void GetInfantryLocations()
+        {
+            int i;
+            for (i = 0; i < infantry.Length; i++)
+            {
+                if (infantry[i].Contains("infantry = {"))
                 {
                     infantryStart = i;
+                    GetEnds(i);
                     isCheckingInfantry = true;
                 }
-                if (isCheckingInfantry && infantry[i].Equals("\t}"))
-                {
-                    infantryEnd = i;
-                    isCheckingInfantry = false;
-                }
-                if (infantry[i].Contains("bicycle_battalion = {"))
+                else if (infantry[i].Contains("bicycle_battalion = {"))
                 {
                     bicycleBattalionStart = i;
-                    isCheckingbicycleBattalion = true;
+                    GetEnds(i);
+                    isCheckingBicycleBattalion = true;
                 }
-                if (isCheckingbicycleBattalion && infantry[i].Equals("\t}"))
+                else if (infantry[i].Contains("marine = {"))
                 {
-                    bicycleBattalionEnd = i;
-                    isCheckingbicycleBattalion = false;
+                    marineStart = i;
+                    GetEnds(i);
+                    isCheckingMarine = true;
+                }
+                else if (infantry[i].Contains("mountaineers = {"))
+                {
+                    mountaineersStart = i;
+                    GetEnds(i);
+                    isCheckingMountaineers = true;
+                }
+                else if (infantry[i].Contains("paratrooper = {"))
+                {
+                    paratrooperStart = i;
+                    GetEnds(i);
+                    isCheckingParatrooper = true;
+                }
+                else if (infantry[i].Contains("motorized = {"))
+                {
+                    motorizedStart = i;
+                    GetEnds(i);
+                    isCheckingMotorized = true;
+                }
+                else if (infantry[i].Contains("mechanized = {"))
+                {
+                    mechanizedStart = i;
+                    GetEnds(i);
+                    isCheckingMechanized = true;
                 }
             }
+            GetEnds(i);
+        }
 
-            int x = 0;
+        private void GetEnds(int location)
+        {
+            if(isCheckingInfantry)
+            {
+                infantryEnd = location - 1;
+                isCheckingInfantry = false;
+            }
+            else if (isCheckingBicycleBattalion)
+            {
+                bicycleBattalionEnd = location - 1;
+                isCheckingBicycleBattalion = false;
+            }
+            else if (isCheckingMarine)
+            {
+                marineEnd = location - 1;
+                isCheckingMarine = false;
+            }
+            else if (isCheckingMountaineers)
+            {
+                mountaineersEnd = location - 1;
+                isCheckingMountaineers = false;
+            }
+            else if (isCheckingParatrooper)
+            {
+                paratrooperEnd = location - 1;
+                isCheckingParatrooper = false;
+            }
+            else if (isCheckingMotorized)
+            {
+                motorizedEnd = location - 1;
+                isCheckingMotorized = false;
+            }
+            else if (isCheckingMechanized)
+            {
+                mechanizedEnd = location - 1;
+                isCheckingMechanized = false;
+            }
         }
     }
 }
